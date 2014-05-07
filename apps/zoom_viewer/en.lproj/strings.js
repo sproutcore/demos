274d7992246091f17eb30a79bf0833e3c4f38cba
James Chao <jameschao@gmail.com>
2014-05-07 07:28:27 -0500

    Added zoom viewer demo

---
+++ apps/zoom_viewer/en.lproj/strings.js
@@ -0,0 +1,12 @@
+// ==========================================================================
+// Project:   ZoomViewer
+// Copyright: @2014 iParadigms, LLC
+// License:   Licensed under MIT license
+// ==========================================================================
+/*globals ColorDemo*/
+
+// American English
+SC.stringsFor('en', {
+  "_DemoTitle": "Zoom Demo",
+  "_DemoDescription": "In this demo we demonstrate the scaling capabilities of SC.ScrollView.<br><br>Key SproutCore technologies used in this demo: SC.ScrollView."
+});
